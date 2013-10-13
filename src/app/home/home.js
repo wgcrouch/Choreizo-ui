f263d2af8468227c37580d4bcf9d385a55ae1a2b
George <george@abstractpoint.co.uk>
2013-10-13 06:31:55 +0100

    New actions and styling

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -15,6 +15,55 @@ angular.module( 'choreizo.home', [
     },
     data:{ pageTitle: 'Home' }
   });
+  
+  //People index
+  $stateProvider.state( 'people', {
+    url: '/people',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/people.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'People' }
+  });
+  
+  //Chores index
+  $stateProvider.state( 'chores', {
+    url: '/chores',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/chores.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Chores' }
+  });
+  
+  //People Add
+  $stateProvider.state( 'people_add', {
+    url: '/people/add',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/people_add.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Add Person' }
+  });
+  
+  //Chores Add
+  $stateProvider.state( 'chores_add', {
+    url: '/chores/add',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/chores_add.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Add Chore' }
+  });
+  
 })
 
 
